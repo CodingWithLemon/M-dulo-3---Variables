632bc60e42cf27c46b036d9854e579f35361108d
Alvaro <alvarocalcala@gmail.com>
2024-04-18 12:51:58 +0200

    Intento

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,41 @@
-import "./style.css";
 
-console.log("Hello Typescript!");
+const grupo1 = {
+    nombre: 'The Beatles',
+    año: 1960,
+    activo: true,
+    genero: '🎵 Pop Rock',
+};
+
+const grupo2 = {
+    nombre: 'Queen',
+    año: 1970,
+    activo: false,
+    genero: '🎸 Rock',
+};
+
+const grupo3 = {
+    nombre: 'AC DC',
+    año: 1973,
+    activo: true,
+    genero: '🤘 Hard Rock',
+};
+
+const grupo4 = {
+    nombre: 'Ludwig van Beethoven',
+    año: 1770,
+    activo: false,
+    genero: '🎼 Clásica',
+};
+
+const grupo5 = {
+    nombre: 'The Rolling Stones',
+    año: 1962,
+    activo: true,
+    genero: '🎸 Rock',
+};
+
+console.log(grupo1)
+console.log(grupo2)
+console.log(grupo3)
+console.log(grupo4)
+console.log(grupo5)
